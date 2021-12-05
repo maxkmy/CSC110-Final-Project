d94b8cf5d92140bea0413df640f7b3858673a7d8
maxkmy <maxkoh03@gmail.com>
2021-12-04 22:41:30 -0500

    Minor changes to import and function calls

--- visualizations.py
+++ visualizations.py
@@ -2,35 +2,34 @@
 """
 
 import plotly.graph_objects as go
-import computations as compute
+import computations
 
 
-def plot_percentage_change(name: str, root: str, start: int, end: int) -> None:
+def plot_percentage_change(country: str, root: str, start: int, end: int) -> None:
     """Plot trend.
 
     Preconditions:
-
         - outputs != {}
-
 
     Sample Usage:
     >>> plot_percentage_change('Australia', 'unemployment', 2016, 2020)
     >>> plot_percentage_change('Canada', 'gdp', 2016, 2020)
 
     """
+    ordered_data = computations.get_percent_change_over_time(root, start, end, country)
+    x_data, y_data = computations.get_xy_data(ordered_data)
 
-    # Convert the outputs into parallel x and y lists
-    x_data, y_data = compute.get_xy_data(compute.get_percent_change_over_time(root, start, end, name))
-
+    yaxis_title = [word.capitalize() for word in (root.split('_'))]
+    yaxis_title = ''.join(yaxis_title)
 
     # Create the figure
     fig = go.Figure()
-    fig.add_trace(go.Scatter(x=x_data, y=y_data, name=name))
+    fig.add_trace(go.Scatter(x=x_data, y=y_data, name=country))
 
     # Configure the figure
-    fig.update_layout(title=f'Annual {root} Percent Change of {name}',
+    fig.update_layout(title=f'Annual {root} Percent Change of {country}',
                       xaxis_title='(Year)',
-                      yaxis_title=f'Calculated Percentage Change')
+                      yaxis_title=f'{yaxis_title} Percentage Change')
 
     # Show the figure in the browser
     fig.show()
