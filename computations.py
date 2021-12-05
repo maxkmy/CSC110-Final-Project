eb3b687d23a71425dbd8e080fc129cb6aa6e4fe2
maxkmy <maxkoh03@gmail.com>
2021-12-04 20:48:30 -0500

    Add computation methods

--- computations.py
+++ computations.py
@@ -1,69 +1,55 @@
 """ Contains functions to perform various computations
 """
 
-from clean_data import Country
+from clean_data import clean_data
+from typing import List
+
+country_dict = clean_data()
 
 
-def percentage_change(new: float, old: float) -> float:
+def percent_change(new: float, old: float) -> float:
     """ Returns the percentage change between old and new
-
-    TODO: add doctest
     """
     return (new - old) / old * 100
 
 
-def percentage_of_whole(portion, whole: float) -> float:
+def percent_of_whole(portion, whole: float) -> float:
     """ Returns the percentage the portion makes up of whole
-
-    TODO: add doctest
-
     """
     return portion / whole * 100
 
 
-# TODO: Add computations for predictor
-
-def percent_change_list(country_data: Country, metric: str) -> list[tuple[float, float]]:
-    """ Return list of tuples representing annual percent change of 'metric' from 2016-2020.
-    The tuple keys represent the year and correponds to the specific percentage change of 'metric'
-    of the year.
-
-    TODO: add doctest
-
+def get_percent_change(root: str, new_attr_suffix: str, old_attr_suffix, country: str) -> float:
+    """ Calculates the percent change between the attributes root + new_attr_suffix and
+    root + old_attr_suffix
     """
-    percent_change_so_far = []
-    if metric == 'gdp':
-        data = [country_data.gdp_2016, country_data.gdp_2017,
-                country_data.gdp_2018, country_data.gdp_2019,
-                country_data.gdp_2020]
-    elif metric == 'unemployment':
-        data = [country_data.unemployment_2016, country_data.unemployment_2017,
-                country_data.unemployment_2018, country_data.unemployment_2019,
-                country_data.unemployment_2020]
-    else:
-        return []
-    if all(len(str(x)) == 0 for x in data):
-        return []
-    year_so_far = 2016
-    for i in range(len(data) - 1):
-        value = percentage_change(data[i + 1], data[i])
-        list.append(percent_change_so_far, (year_so_far, value))
-        year_so_far += 1
-    return percent_change_so_far
+    country = country_dict[country]
+    new_attr = root + new_attr_suffix
+    old_attr = root + old_attr_suffix
+    new = getattr(country, new_attr)
+    old = getattr(country, old_attr)
+    return percent_change(new, old)
 
 
-def get_xy_data(ordered_data: list[tuple[float, float]]) -> tuple[list[int], list[float]]:
-    """Return a tuple of two parallel lists. The first list contains the keys of outputs as
-        ints representing the year. The second list contains the corresponding value of
-        the attribute of the specific metric for data (gdp or unemployment % change)
-
-    TODO: add doctest
+def get_percent_change_over_time(root: str, start: int, end: int, country: str) -> List[float]:
+    """ Calculates the percentage change of the desired attribute (root) between adjacent years
+    rangging from start to end inclusive
     """
-    # ACCUMULATOR year_so_far: strings from outputs
-    year_so_far = []
-    # ACCUMULATOR outputs_so_far: floats from outputs
-    data_so_far = []
-    for put in ordered_data:
-        list.append(year_so_far, put[0])
-        list.append(data_so_far, put[1])
-    return year_so_far, data_so_far
+    percentage_changes = []
+    for year in range(start, end):
+        cur = str(year)
+        nxt = str(year + 1)
+        percentage_changes.append(get_percent_change(root, nxt, cur, country))
+    return percentage_changes
+
+
+def aggregate(attribute: str) -> float:
+    """ Calculates the sum of the attribute in country_dict where the attribute is available
+    (i.e. the attribute is not an empty string)
+    """
+    accum = 0
+    for country in country_dict:
+        to_add = getattr(country_dict[country], attribute)
+        if to_add != '':
+            accum += to_add
+    return accum
