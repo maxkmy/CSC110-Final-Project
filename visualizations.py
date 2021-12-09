d55c2d3a81b3ef11124295ef9529307762faacb7
maxkmy <maxkoh03@gmail.com>
2021-12-08 20:07:02 -0500

    Improve efficiency, remove try-catch, rename variables, use object notation

--- visualizations.py
+++ visualizations.py
@@ -3,60 +3,55 @@
 
 import plotly.graph_objects as go
 
-import pandas as pd
-
 import clean_data
 import computations
 
 
 def plot_percentage_change(root: str, start: int, end: int) -> None:
-    """Plot trend.
+    """Plot percentage change of desired attribute (root) over the years start to end inclusive.
 
     Preconditions:
-        - outputs != {}
+        - 0 < start < end
 
     Sample Usage:
     >>> plot_percentage_change('gdp_', 2016, 2020)
     >>> plot_percentage_change('unemployment_', 2016, 2020)
 
     """
-
-    countries = [clean_data.clean_data()[x] for x in clean_data.clean_data()]
+    # get cleaned data
+    country_dict = clean_data.clean_data()
+    countries = [country_dict[country] for country in country_dict]
 
     # Create the figure
     fig = go.Figure()
-    counter = 0
     country_list = []
     for country in countries:
-        try:
-            ordered_data = computations.get_percent_change_over_time(root, start, end, country.name)
-            x_data, y_data = computations.get_xy_data(ordered_data)
+        # get ordered pair in the form (year, percentage change) and split into x and y data
+        ordered_data = computations.get_percent_change_over_time(root, start, end, country.name)
+        x_data, y_data = computations.get_xy_data(ordered_data)
+        # add the plot to fig and append country to country_list
+        fig.add_trace(go.Scatter(x=x_data, y=y_data, name=country.name))
+        country_list.append(country.name)
 
-            fig.add_trace(go.Scatter(x=x_data, y=y_data, name=country.name))
-            list.append(country_list, country.name)
-            counter += 1
-        except TypeError:
-            pass
-
+    # set y-axis title
     yaxis_title = [word.capitalize() for word in (root.split('_'))]
-    yaxis_title = ''.join(yaxis_title)
+    yaxis_title = ' '.join(yaxis_title)
 
     # Configure the figure
-    fig.update_layout(title=f'Annual {yaxis_title} Percent Change of Selected Country',
+    fig.update_layout(title=f'Annual {yaxis_title} Percent Change of All Countries',
                       xaxis_title='Year',
                       yaxis_title=f'{yaxis_title} Percentage Change')
 
-    # Add dropdown
-
-    buttons_so_far = [dict(
+    # Add dropdown buttons
+    buttons = [dict(
         label='All',
         method='update',
-        args=[{'visible': [True for x in range(len(country_list))]},
+        args=[{'visible': [True for _ in range(len(country_list))]},
               {'title': f'Annual {yaxis_title} Percent Change of Countries',
                'showlegend': True}])]
 
     for i in range(len(country_list)):
-        list.append(buttons_so_far, dict(
+        buttons.append(dict(
             label=country_list[i],
             method='update',
             args=[{'visible': [x == i for x in range(len(country_list))]},
@@ -66,12 +61,8 @@ def plot_percentage_change(root: str, start: int, end: int) -> None:
     fig.update_layout(
         updatemenus=[go.layout.Updatemenu(
             active=0,
-            buttons=buttons_so_far
-        )
-
-        ]
+            buttons=buttons
+        )]
     )
 
     fig.show()
-
-
