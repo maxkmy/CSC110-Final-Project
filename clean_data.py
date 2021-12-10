579f454d0d46cc3a2686080c6af5bbf6a3f6043f
maxkmy <maxkoh03@gmail.com>
2021-12-10 12:18:14 -0500

    Keep code_dict

--- clean_data.py
+++ clean_data.py
@@ -94,7 +94,7 @@ class Country:
         return self.name
 
 
-def populate_dictionary() -> dict[str, Country]:
+def populate_dictionary() -> tuple[dict[str, Country], dict[str, str]]:
     """ Return a tuple of dictionary. The first dictionary maps the country code to a country name
     and the second dictionary maps the country name to a Country instance.
     """
@@ -112,11 +112,12 @@ def populate_dictionary() -> dict[str, Country]:
             # get the country's name and country code
             name = row[0].capitalize()
             code = row[1]
-            # map the name to a Country instance
+            # map the name to a Country instance and the code to the name
             country_dict[name] = Country(name)
+            code_dict[name] = code
 
-    # return accumulator
-    return country_dict
+    # return both accumulators
+    return country_dict, code_dict
 
 
 def populate_attribute_name(country_dict: dict, filename: str, lines: int, attributes: [str],
@@ -238,7 +239,7 @@ def clean_data() -> dict[str, Country]:
     """ Main method that contains helper function calls to clean data
     """
     # populate the dictionary with country names and Country instances
-    country_dict = populate_dictionary()
+    country_dict = populate_dictionary()[0]
     # get required attributes from csv files
     get_national_gdp(country_dict)
     get_gdp_quartile(country_dict)
